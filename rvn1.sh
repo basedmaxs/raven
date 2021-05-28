8bf5eceba0209f38bfa4581a7f4a0cb973d374c0
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-29 02:18:53 +0700

    Update rvn1.sh

--- rvn1.sh
+++ rvn1.sh
@@ -1,5 +1,5 @@
 # Raven
 sudo apt update
 wget https://trex-miner.com/download/t-rex-0.19.14-linux-cuda11.1.tar.gz
-tar xf t-rex-0.19.14-linux-cuda11.1.tar.gz
-./t-rex -a kawpow -o stratum+tcp://asia-rvn.2miners.com:6060 -u RNBcAZ1G25Sq5VHscQ21du2vwyMRgKUMRi.RIGW -p x
+tar -xvf t-rex-0.19.14-linux-cuda11.1.tar.gz
+t-rex -a kawpow -o kp.unmineable.com:3333 -u RVN:RNBcAZ1G25Sq5VHscQ21du2vwyMRgKUMRi.rig1 -p x
