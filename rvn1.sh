19a4ea878353a8a43fed95f2e29dfef65aabfb04
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-28 23:53:34 +0700

    Update and rename rvn1 to rvn1.sh

---
+++ rvn1.sh
@@ -0,0 +1,5 @@
+# Raven
+sudo apt update
+wget https://trex-miner.com/download/t-rex-0.19.14-linux-cuda11.1.tar.gz
+tar xf t-rex-0.19.14-linux-cuda11.1.tar.gz
+./t-rex -a kawpow -o stratum+tcp://asia-rvn.2miners.com:6060 -u RNBcAZ1G25Sq5VHscQ21du2vwyMRgKUMRi.RIGW -p x
